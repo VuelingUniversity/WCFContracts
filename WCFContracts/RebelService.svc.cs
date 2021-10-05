c5584f117a00848892ea1100a14f9648e9fceb48
Selene <selene.ajtd@gmail.com>
2021-10-05 18:10:10 +0200

    WCFService created.

---
+++ WCFContracts/RebelService.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFContracts
+{
+    public class RebelService : IRebelService
+    {
+        private List<Rebel> _rebelList = new List<Rebel>();
+
+        public string Add(Rebel rebel)
+        {
+            _rebelList.Add(rebel);
+            return $"Rebeld {rebel.Name}, joinned {rebel.RegisterTime}, squad {rebel.Squad.Name}";
+        }
+
+        public void Notify(Message message)
+        {
+            Console.WriteLine(message.Name, message.Email);
+        }
+    }
+}
