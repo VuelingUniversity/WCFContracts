bcf360ac5ebcc901fbeb932fc3fb0770693e421d
tdom91 <tomasdominik@outlook.com>
2021-10-06 10:48:42 +0200

    Solucion

---
+++ Contract/IService1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Contract
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract(Name = "AddRebel")]
+        void Add(Rebel rebel);
+
+        [OperationContract(IsOneWay = true)]
+        void SendNotification(string name, string email);
+    }
+    [DataContract]
+    public class Rebel
+    {
+        public Squad _squad;
+        public GeoLocation _location;
+        public string _name;
+        public DateTime _datetime = DateTime.Now;
+
+        [DataMember]
+        public Squad Squad
+        {
+            get { return _squad; }
+            set { _squad = value; }
+        }
+
+        [DataMember]
+        public GeoLocation GeoLocation
+        {
+            get { return _location; }
+            set { _location = value; }
+        }
+
+        [DataMember(Name = "Name")]
+        public string RebelName
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Name = "dateTime")]
+        public DateTime JoinningDate
+        {
+            get { return _datetime; }
+            set { _datetime = value; }
+        }
+    }
+    [DataContract]
+    public class Squad
+    {
+        public string _nameSquad;
+        public string _location;
+        public DateTime _datetime = DateTime.Now;
+        [DataMember]
+        public string Name
+        {
+            get { return _nameSquad; }
+            set { _nameSquad = value; }
+        }
+        [DataMember]
+        public string Location
+        {
+            get { return _location; }
+            set { _location = value; }
+        }
+        [DataMember]
+        public DateTime Date
+        {
+            get { return _datetime; }
+            set { _datetime = value; }
+        }
+    }
+    [DataContract]
+    public class GeoLocation
+    {
+        public int _x;
+        public int _y;
+        [DataMember]
+        public int CoordX
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+        [DataMember]
+        public int CoordY
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+    }
+    [DataContract(Name = "SendNotification")]
+    public class Notify
+    {
+        public string _name;
+        public string _email;
+        [DataMember(Order = 1)]
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        [DataMember(Order = 0)]
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+    }
+}
---
+++ Contract/Service1.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Contract
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {           
+        private List<Rebel> newRebel = new List<Rebel>();
+        public void Add(Rebel rebel)
+        {
+            newRebel.Add(rebel);
+        }
+
+        public void SendNotification(string name, string email)
+        {
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("clafuente@gteam.com", "Teruel09"),
+                EnableSsl = true,
+            };
+            string body = $"Hola {name} este es tu email";
+            smtpClient.Send("clafuente@gteam.com", email, "subject", body);
+        }
+    }
+}
+
